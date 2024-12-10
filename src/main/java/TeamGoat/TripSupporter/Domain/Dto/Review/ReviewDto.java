129b2f2b902851e6deebd9f888200c85d5084aee
bong123123 <bongcheonjy@naver.com>
2024-12-10 15:36:02 +0900

    errorfix

--- src/main/java/TeamGoat/TripSupporter/Domain/Dto/Review/ReviewDto.java
+++ src/main/java/TeamGoat/TripSupporter/Domain/Dto/Review/ReviewDto.java
@@ -10,10 +10,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-<<<<<<< HEAD
 @ToString
-=======
->>>>>>> eb2400e6ef8985be4db8b9249b3db945c5ea5104
 public class ReviewDto {
     private Long reviewId;
 
