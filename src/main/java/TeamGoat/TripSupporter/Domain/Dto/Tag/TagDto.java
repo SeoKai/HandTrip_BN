42d1656a56afd7accd2a9a3791502963ee4d63b4
Gotchun <qkreogo1@naver.com>
2024-12-10 09:20:11 +0900

    feat:dto,entity add

---
+++ src/main/java/TeamGoat/TripSupporter/Domain/Dto/Tag/TagDto.java
@@ -0,0 +1,16 @@
+package TeamGoat.TripSupporter.Domain.Dto.Tag;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TagDto {
+
+    private Long tagId;
+
+    private String tagName;
+
+}
