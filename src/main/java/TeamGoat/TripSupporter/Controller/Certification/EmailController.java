861a64b6ad8ef486cc02a1c2909e8a8b930e58d5
bong123123 <bongcheonjy@naver.com>
2024-12-27 13:46:45 +0900

    메일보내기 추가

--- src/main/java/TeamGoat/TripSupporter/Controller/Certification/EmailController.java
+++ src/main/java/TeamGoat/TripSupporter/Controller/Certification/EmailController.java
@@ -1,10 +1,14 @@
 package TeamGoat.TripSupporter.Controller.Certification;
 
 import TeamGoat.TripSupporter.Service.Certification.EmailService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/email")
+@Slf4j
 public class EmailController {
 
     private final EmailService emailService;
@@ -14,8 +18,22 @@ public class EmailController {
     }
 
     @PostMapping("/send")
-    public String sendEmail(@RequestParam String to, @RequestParam String subject, @RequestParam String body) {
-        emailService.sendVerificationEmail(to);
+    public String sendEmail(@RequestBody Map<String, String> payload) {
+        log.info("인증메일 전송");
+        String userEmail = payload.get("userEmail");
+        emailService.sendVerificationEmail(userEmail);
         return "인증 이메일이 발송되었습니다.";
+    }
+
+    @PostMapping("/verify")
+    public String verifyCode(@RequestBody Map<String, String> payload) {
+        String userEmail = payload.get("userEmail");
+        String code = payload.get("code");
+        boolean isVerified = emailService.verifyCode(userEmail, code);
+        if (isVerified) {
+            return "인증에 성공했습니다.";
+        } else {
+            return "인증에 실패했습니다.";
+        }
     }
 }
